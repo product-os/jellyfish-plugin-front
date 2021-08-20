9914155598b4dc739b8b6b087bf577b5ae73dd1f
Josh Bowling <josh@balena.io>
2021-08-20 18:09:20 +0900

    Bump action-library and jellyfish-types
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/unit/actions/action-integration-front-mirror-event.spec.ts
+++ test/unit/actions/action-integration-front-mirror-event.spec.ts
@@ -4,11 +4,8 @@
  * Proprietary and confidential.
  */
 
-import type {
-	ActionRequestData,
-	Context,
-	Contract,
-} from '@balena/jellyfish-types/build/core';
+import { WorkerContext } from '@balena/jellyfish-types/build/worker';
+import type { Context, Contract } from '@balena/jellyfish-types/build/core';
 import isArray from 'lodash/isArray';
 import { v4 as uuidv4 } from 'uuid';
 import { actionIntegrationFrontMirrorEvent } from '../../../lib/actions/action-integration-front-mirror-event';
@@ -44,30 +41,6 @@ function makeContract(type: string, data = {}, slug = ''): Contract {
 }
 
 /**
- * @summary Generate and return a message contract
- * @function
- * @param context - execution context
- * @param data - optional contract data object
- * @returns message contract
- */
-function makeMessage(ctx: Context, data = {}): Contract {
-	return makeContract(
-		'message',
-		Object.assign(
-			{},
-			{
-				actor: ctx.actor.id,
-				payload: {
-					message: uuidv4(),
-				},
-				timestamp: new Date().toISOString(),
-			},
-			data,
-		),
-	);
-}
-
-/**
  * @summary Generate and return an action request object
  * @function
  *
@@ -75,29 +48,36 @@ function makeMessage(ctx: Context, data = {}): Contract {
  * @param requestArguments - optional request arguments
  * @returns action request object
  */
-function makeRequest(ctx: Context, requestArguments = {}): ActionRequestData {
+function makeRequest(args = {}): any {
 	return {
-		context: {
-			id: `TEST-${uuidv4()}`,
-		},
-		timestamp: new Date().toISOString(),
-		actor: ctx.actor.id,
-		originator: uuidv4(),
-		arguments: requestArguments,
-		epoch: 1,
-		input: {
+		action: {
 			id: uuidv4(),
+			name: uuidv4(),
+			slug: `action-${uuidv4()}`,
+			type: 'action@1.0.0',
+			version: '1.0.0',
+			active: true,
+			links: {},
+			tags: [],
+			markers: [],
+			created_at: new Date().toISOString(),
+			requires: [],
+			capabilities: [],
+			data: {
+				arguments: args,
+			},
 		},
-		action: 'test',
+		card: uuidv4(),
+		actor: uuidv4(),
+		context: {} as Context,
+		timestamp: new Date().toISOString(),
+		epoch: new Date().toISOString(),
+		arguments: args,
+		originator: uuidv4(),
 	};
 }
 
-const context: Context = {
-	id: `test-${uuidv4()}`,
-	session: uuidv4(),
-	actor: {
-		id: uuidv4(),
-	},
+const context: any = {
 	getCardById: () => {
 		return {
 			id: uuidv4(),
@@ -117,10 +97,10 @@ describe('action-integration-front-mirror-event', () => {
 	test('should return a list of cards', async () => {
 		expect.assertions(1);
 		const result = await handler(
-			context.session,
-			context,
-			makeMessage(context),
-			makeRequest(context),
+			`test-${uuidv4()}`,
+			context as WorkerContext,
+			makeContract('user'),
+			makeRequest(),
 		);
 		if (isArray(result)) {
 			expect(Object.keys(result[0])).toEqual(['id', 'type', 'version', 'slug']);
