856e87be73c5f3d54651f137c4684fe8216cdb93
Josh Bowling <josh@balena.io>
2021-11-21 08:28:41 +0000

    Remove license headers
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/unit/actions/action-integration-front-mirror-event.spec.ts
+++ test/unit/actions/action-integration-front-mirror-event.spec.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
 import { WorkerContext } from '@balena/jellyfish-types/build/worker';
 import type { Context, Contract } from '@balena/jellyfish-types/build/core';
 import isArray from 'lodash/isArray';
